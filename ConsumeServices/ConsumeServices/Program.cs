de280f482dd00e7a2a0da9f46816dd145f597cb6
AlonsoMT <d.alonsomt@gmail.com>
2017-01-04 11:43:29 -0500

    - Se agrega el proyecto de consumo del servicio de prueba - Se crea la clase encargada de realizar las tareas de negocio en el servicio web llamada "Contact"

---
+++ ConsumeServices/ConsumeServices/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumeServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+        }
+
+        static async Task RunAsync()
+        {
+            var client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://localhost:64944/");
+
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            HttpResponseMessage response = await client.GetAsync("/api/contact");
+
+            if (response.IsSuccessStatusCode)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
